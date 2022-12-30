2bfc18fe801e1884ec4a15d8966e599198699d76
xiexuefeng <1158231926@qq.com>
2022-12-30 17:07:49 +0800

    feat(ping-pong): 物体间物理交互测试

--- src/demos/ping-pong/PingPong.vue
+++ src/demos/ping-pong/PingPong.vue
@@ -4,12 +4,18 @@
 
 <script lang="ts" setup>
 import type { Quaternion, Vector3 } from 'three'
+import { PointLight } from 'three'
 import { AmbientLight, DirectionalLight, MeshPhongMaterial } from 'three'
 import { Clock } from 'three'
 import { BoxGeometry} from 'three'
-import { DoubleSide, Mesh, MeshBasicMaterial, PerspectiveCamera, PlaneGeometry, Scene, SphereGeometry, WebGLRenderer } from 'three'
+import { DoubleSide, Mesh, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import AmmoInit from '@/plugins/ammo/ammo.wasm'
+import { random } from 'lodash-es'
+import { rightEvent } from '@/plugins/joy-con'
+import type { CommonInput } from '@/typings/joy-con'
+
+type BodyFunction = (body: any) => void
 
 const container = ref<HTMLElement>()
 const rigidBodies: Mesh[] = []
@@ -18,18 +24,24 @@ const MARGIN = 0.05
 let Ammo: any = null
 let physicsWorld: any
 let transformAux1: any
+let ball: Mesh
+let box: Mesh
 
 const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 const scene = new Scene()
 const clock = new Clock()
 const renderer = new WebGLRenderer({ antialias: true })
-const sun = new DirectionalLight(0xffffff, 0.8)
+const sun = new DirectionalLight(0xffffff, 0.7)
+const light = new PointLight(0xffffff, 1.0, 50)
 const ambient = new AmbientLight(0xffffff, 0.1)
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true
 camera.lookAt(0, 0, 0)
-camera.position.set(0, 0, 20)
+camera.position.set(0, 0, 10)
+light.position.set(0, 0, 5)
+light.castShadow = true
 sun.position.set(10, 10, 10)
-scene.add(sun, ambient)
+scene.add(sun, light, ambient)
 
 const control = new OrbitControls(camera, renderer.domElement)
 
@@ -37,6 +49,7 @@ function loop () {
   const deltaTime = clock.getDelta()
   updatePhysics(deltaTime)
   control.update()
+  camera.lookAt(box.position)
   renderer.render(scene, camera)
   requestAnimationFrame(loop)
 }
@@ -56,28 +69,149 @@ function initPhysics() {
   transformAux1 = new Ammo.btTransform()
 }
 
-function initObject() {
-  const floor = new Mesh(
-    new BoxGeometry(20, 40, 0.1),
+function initRoom() {
+  const sx = 20
+  const sy = 10
+  const sz = 40
+
+  const left = new Mesh(
+    new BoxGeometry(0.1, sy, sz),
     new MeshPhongMaterial({ color: 0xcccccc, side: DoubleSide })
   )
-  const floorShape = new Ammo.btBoxShape( new Ammo.btVector3( 20 * 0.5, 40 * 0.5, 0.1 * 0.5 ) )
-  floorShape.setMargin(MARGIN)
-  floor.position.set(0, 0, 0)
-  floor.rotateX(90)
-  const ball = new Mesh(
+  const leftShape = new Ammo.btBoxShape( new Ammo.btVector3(0.1 * 0.5, sy * 0.5, sz * 0.5) )
+  left.position.set(-sx * 0.5, 0, 0)
+  left.receiveShadow = true
+  createRigidBody(left, leftShape, 0, left.position, left.quaternion)
+
+  const right = new Mesh(
+    new BoxGeometry(0.1, sy, sz),
+    new MeshPhongMaterial({ color: 0xcccccc, side: DoubleSide })
+  )
+  const rightShape = new Ammo.btBoxShape( new Ammo.btVector3(0.1 * 0.5, sy * 0.5, sz * 0.5) )
+  right.position.set(sx * 0.5, 0, 0)
+  right.receiveShadow = true
+  createRigidBody(right, rightShape, 0, right.position, right.quaternion)
+
+  const top = new Mesh(
+    new BoxGeometry(sx, 0.1, sz),
+    new MeshPhongMaterial({ color: 0xcccccc, side: DoubleSide })
+  )
+  const topShape = new Ammo.btBoxShape( new Ammo.btVector3(sx * 0.5, 0.1 * 0.5, sz * 0.5) )
+  top.position.set(0, sy * 0.5, 0)
+  top.receiveShadow = true
+  createRigidBody(top, topShape, 0, top.position, top.quaternion)
+
+  const bottom = new Mesh(
+    new BoxGeometry(sx, 0.1, sz),
+    new MeshPhongMaterial({ color: 0xcccccc, side: DoubleSide })
+  )
+  const bottomShape = new Ammo.btBoxShape( new Ammo.btVector3(sx * 0.5, 0.1 * 0.5, sz * 0.5) )
+  bottom.position.set(0, -sy * 0.5, 0)
+  bottom.receiveShadow = true
+  createRigidBody(bottom, bottomShape, 0, bottom.position, bottom.quaternion)
+
+  const front = new Mesh(
+    new BoxGeometry(sx, sy, 0.1),
+    new MeshPhongMaterial({ color: 0xcccccc, side: DoubleSide })
+  )
+  const frontShape = new Ammo.btBoxShape( new Ammo.btVector3(sx * 0.5, sy * 0.5, 0.1 * 0.5) )
+  front.position.set(0, 0, sz * 0.5)
+  front.receiveShadow = true
+  createRigidBody(front, frontShape, 0, front.position, front.quaternion)
+
+  const back = new Mesh(
+    new BoxGeometry(sx, sy, 0.1),
+    new MeshPhongMaterial({ color: 0xcccccc, side: DoubleSide })
+  )
+  const backShape = new Ammo.btBoxShape( new Ammo.btVector3(sx * 0.5, sy * 0.5, 0.1 * 0.5) )
+  back.position.set(0, 0, -sz * 0.5)
+  back.receiveShadow = true
+  createRigidBody(back, backShape, 0, back.position, back.quaternion)
+}
+
+function initObject() {
+  initRoom()
+  ball = new Mesh(
     new SphereGeometry(0.2, 32, 32),
     new MeshPhongMaterial({ color: 0xFFBA26, side: DoubleSide })
   )
   const ballShape = new Ammo.btSphereShape( 0.2 )
   ballShape.setMargin(MARGIN)
-  ball.position.set(0, 5, 0)
+  ball.position.set(0, 3, 0)
+  ball.castShadow = true
+  box = new Mesh(
+    new BoxGeometry(1, 1, 1),
+    new MeshPhongMaterial({ color: 0xFF4A26 })
+  )
+  const boxShape = new Ammo.btBoxShape(new Ammo.btVector3(1 * 0.5, 1 * 0.5, 1 * 0.5))
+  boxShape.setMargin(MARGIN)
+  box.castShadow = true
 
-  createRigidBody(floor, floorShape, 0, floor.position, floor.quaternion)
-  createRigidBody(ball, ballShape, 1, ball.position, ball.quaternion)
+  createRigidBody(ball, ballShape, 1, ball.position, ball.quaternion, (ballBody) => {
+    ballBody.setRestitution(0.8) // 碰撞后补偿系数，系数越大越容易反弹
+    ballBody.setLinearVelocity(
+      new Ammo.btVector3(3, 5, -6)
+    )
+    console.log(ballBody)
+  })
+  createRigidBody(box, boxShape, 10, box.position, box.quaternion)
 }
 
-function createRigidBody(object: Mesh, shape: any, mass: number, pos: Vector3, quat: Quaternion) {
+function initEvent() {
+  window.addEventListener('keydown', e => {
+    const ballBody = ball.userData.physicsBody
+    const boxBody = box.userData.physicsBody
+    const motionState = boxBody.getMotionState()
+    motionState.getWorldTransform(transformAux1)
+    const boxOrigin = transformAux1.getOrigin()
+
+    switch(e.code) {
+    case 'Space':
+      ballBody.applyCentralForce(new Ammo.btVector3(
+        random(-100, 100, true),
+        random(50, 150, true),
+        random(-100, 100, true)
+      ))
+      break
+    case 'ArrowLeft':
+      transformAux1.setOrigin(new Ammo.btVector3(boxOrigin.x() - 0.1, boxOrigin.y(), boxOrigin.z()))
+      boxBody.setWorldTransform(transformAux1)
+      break
+    case 'ArrowRight':
+      transformAux1.setOrigin(new Ammo.btVector3(boxOrigin.x() + 0.1, boxOrigin.y(), boxOrigin.z()))
+      boxBody.setWorldTransform(transformAux1)
+      break
+    case 'ArrowUp':
+      transformAux1.setOrigin(new Ammo.btVector3(boxOrigin.x(), boxOrigin.y(), boxOrigin.z() - 0.1))
+      boxBody.setWorldTransform(transformAux1)
+      break
+    case 'ArrowDown':
+      transformAux1.setOrigin(new Ammo.btVector3(boxOrigin.x(), boxOrigin.y(), boxOrigin.z() + 0.1))
+      boxBody.setWorldTransform(transformAux1)
+      break
+    default:
+      break
+    }
+  })
+
+  rightEvent.addEventListener('sensor-input', e => {
+    const { detail } = e as CustomEvent<CommonInput>
+    if (!detail.actualAccelerometer) {
+      return
+    }
+    const boxBody = box.userData.physicsBody
+    const force = new Ammo.btVector3(
+      detail.actualAccelerometer.x * 9.8 * 10 * 50,
+      detail.actualAccelerometer.y * 9.8 * 10 * 50,
+      detail.actualAccelerometer.z * 9.8 * 10 * 50
+    )
+    // console.log(force.length())
+
+    boxBody.applyCentralForce(force)
+  })
+}
+
+function createRigidBody(object: Mesh, shape: any, mass: number, pos: Vector3, quat: Quaternion, bodyFun?: BodyFunction) {
   object.position.copy(pos)
   object.quaternion.copy(quat)
 
@@ -92,7 +226,12 @@ function createRigidBody(object: Mesh, shape: any, mass: number, pos: Vector3, q
   const rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, shape, localInertia )
   const body = new Ammo.btRigidBody( rbInfo )
   // console.log(body)
-  body.setRestitution(0.75) // 碰撞后补偿系数，系数越大越容易反弹
+
+  if (bodyFun) {
+    bodyFun(body)
+  } else {
+    body.setRestitution(0.75)
+  }
 
   object.userData.physicsBody = body
 
@@ -133,6 +272,7 @@ onMounted(async () => {
   console.log(Ammo)
   initPhysics()
   initObject()
+  initEvent()
   loop()
 })
 </script>
